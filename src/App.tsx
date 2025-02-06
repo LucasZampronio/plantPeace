012b301be4b8fd4b2010316c51713949941b65cc
Fernando Glaeser <fernando.glaesersilva@gmail.com>
2025-02-06 17:21:04 -0300

    feat: minor improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
+import {RegisterForm} from './pages/register.tsx'; 
 
 function App() {
+  const handleSignUp = (data: {
+    name: string;
+    email: string;
+    password: string;
+
+  }) => {
+    console.log("Dados do formulário:", data);
+  };
 
   return (
-    <>
-      <p className='text-2xl text-red-700'>teste</p>
-    </>
-  )
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <RegisterForm onSubmit={handleSignUp} />
+    </div>
+  );
 }
 
-export default App
+export default App;
