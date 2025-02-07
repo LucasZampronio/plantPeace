2537f477af8b5b224995161a31a52c29e3f2dd8f
Manuella Rodrigues <manuzokas@gmail.com>
2025-02-07 04:28:39 -0300

    fix edit plant page and link requests with json-server

--- src/pages/PlantsEditPage.tsx
+++ src/pages/PlantsEditPage.tsx
@@ -1,13 +1,89 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom"; // Para acessar o ID da planta na URL
+import {
+  SignedIn,
+  SignedOut,
+  RedirectToSignIn,
+  useAuth,
+} from "@clerk/clerk-react";
 import PlantForm from "../components/PlantsForm";
+import { PlantFormData } from "../hooks/usePlantForm";
+
 
 export default function PlantsEditPage() {
+  const { userId } = useAuth();
+  const { id } = useParams();
+  const [initialData, setInitialData] = useState<Partial<PlantFormData> | null>(
+    null
+  );
+  const navigate = useNavigate();
+
+  // Verifique se o `plantId` está sendo passado corretamente
+  useEffect(() => {
+    if (!id) {
+      console.error("Erro: plantId não foi encontrado na URL.");
+      return;
+    }
+
+    const loadPlantData = async () => {
+      try {
+        const response = await fetch(`http://localhost:3001/plants/${id}`);
+        if (!response.ok) {
+          throw new Error("Erro ao carregar os dados da planta");
+        }
+        const data = await response.json();
+        setInitialData(data);
+      } catch (error) {
+        console.error("Erro:", error);
+      }
+    };
+
+    loadPlantData();
+  }, [id]);
+
+  // Função que será chamada quando o formulário for enviado
+  const handleSubmit = async (plantData: PlantFormData) => {
+    try {
+      const response = await fetch(`http://localhost:3001/plants/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          ...plantData,
+          ownerId: userId,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao editar a planta");
+      }
+
+      const data = await response.json();
+      console.log("Planta editada com sucesso:", data);
+
+      navigate("/");
+    } catch (error) {
+      console.error("Erro:", error);
+    }
+  };
+
   return (
-    <PlantForm
-      mode="edit"
-      // initialData={plantData}
-      onSubmit={async (data) => {
-        console.log(data);
-      }}
-    />
+    <>
+      <SignedIn>
+        {initialData ? (
+          <PlantForm
+            mode="edit"
+            initialData={initialData}
+            onSubmit={handleSubmit}
+          />
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
   );
 }
