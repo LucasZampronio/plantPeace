5dbb35cf7a7554b579f2d24965db737f3cbf20e8
Lucas <lucas10609@gmail.com>
2025-02-06 23:26:47 -0300

    feat:('Adding users to json server db')

--- src/components/editAccount/edit.tsx
+++ src/components/editAccount/edit.tsx
@@ -1,17 +1,47 @@
-import './edit.css'
+import './edit.css'      
+{/*Vou usar inline mesmo*/} 
 import { useState } from 'react';
 
-const edit = () => {
-/*VOU TENTAR REFAZER DEPOIS, NÃO ENTENDI MUITO BEM O JSONSERVER E COMO VOU FAZER PARA ALTERAR OS DADOS*/
-  const [name,setName] = useState<string>('');
-  const [email,setEmail] = useState<string>('');
-  const [password,setPassword] = useState<string>('');
+const edit: React.FC = () => {
+  {/*Ao inves de criar, buscar por id e renomear os dados*/}
+  const [nome, setNome] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [senha, setSenha] = useState<string>('');
+
+ 
+  const handleNomeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNome(e.target.value);
+  };
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
 
-  const handleSubmit  = (e:React.FormEvent) => {
+  const handleSenhaChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSenha(e.target.value);
+  };
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
-    alert(' Nome: ' + name + ' Email: ' + email +' Senha: ' + password);
+    const usuario = { nome, email, senha }; 
+      {/*API*/} 
+    const response = await fetch('http://localhost:3001/usuarios', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(usuario), 
+    });
+
+  
+    if (response.ok) {
+      const data = await response.json(); 
+      alert(`Usuário criado com sucesso: ${data.nome} Email:${data.email} Senha:${data.senha}`); 
+    } else {
+     
+      alert('Erro ao criar usuário'); 
+    }
   };
 
 
@@ -30,13 +60,13 @@ const edit = () => {
           <form onSubmit={handleSubmit} className="flex flex-col gap-2 rounded-2xl bg-white mt-10x   w-full  pl-15 pr-10" >
             {/*CAMPO DE NOME*/}
             <p>Name</p>
-            <input type="name" placeholder="Echinocereus Cactus" onChange={(e) => setName(e.target.value)}className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
+            <input type="name" value={nome} placeholder="Echinocereus Cactus" onChange={handleNomeChange}className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
             {/*CAMPO DE EMAIL*/}
             <p>E-mail</p>
-            <input type="e-mail" placeholder="A majestic addition to your plant collection" onChange={(e) => setEmail(e.target.value)} className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
+            <input type="e-mail" value={email} placeholder="A majestic addition to your plant collection" onChange={handleEmailChange} className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
             {/*CAMPO DE SENHA*/}
             <p>Password</p>
-            <input type="password" placeholder="***********" onChange={(e) => setPassword(e.target.value)} className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
+            <input type="password" value={senha} placeholder="***********" onChange={handleSenhaChange} className="mt- py-2 px-6 bg-[#F1F5F9] text-[#334155] border-[#E2E8F0] border-2 placeholder-[#64748B] rounded-lg"/>
             {/*BOTÃO DE ENVIAR*/}
             <button className="mt-4 py-2 px-6 bg-[#064E3B] text-white rounded-lg hover:bg-[#267355] transition-colors"> Edit account</button>
           </form>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
       './src/**/*.{js,ts,jsx,tsx}', 
     ],
     theme: {
-      extend: {},
-    },
+      extend: {
+        inter: ['Inter', 'sans-serif']
+      },
+    }, 
     plugins: [],
   };
+
   
