17e802aa1e6dabc944b89db81be9cd1906cd7b5a
Lucas <lucas10609@gmail.com>
2025-02-10 13:25:54 -0300

    Feat:('Error 403 page')

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
       {
         path: "/about-us",
         element: (
-            <AboutUsPage />
+            <AboutUsPage/>
         ),
       },
       {
