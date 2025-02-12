df000ed5621ec69ee7727b72dcefe106e4c2ac7d
Manuella Rodrigues <manuzokas@gmail.com>
2025-02-12 04:40:10 -0300

    custom toggle theme button add new layout

--- src/components/DarkMode.tsx
+++ src/components/DarkMode.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const DarkMode = () => {
   const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
@@ -20,50 +21,26 @@ const DarkMode = () => {
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
 
   return (
-    <div className="flex p-4 border-1 rounded-full border-yellow-600 dark:border-gray-200">
+    <div className="flex py-4 transition-colors duration-300">
       <button
         onClick={toggleDarkMode}
-        className="transition-colors duration-300 cursor-pointer"
+        className={`relative w-20 h-10 rounded-full focus:outline-none transition-all duration-300 ${
+          isDarkMode ? "bg-gray-800" : "bg-yellow-50"
+        }`}
         aria-label={
           isDarkMode ? "Alternar para modo claro" : "Alternar para modo escuro"
         }
       >
-        <div className="relative w-6 h-6 ">
-          {/* Ícone do Sol */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`absolute w-6 h-6 transition-all duration-300   text-yellow-600 ${
-              isDarkMode ? "opacity-0 rotate-90" : "opacity-100 rotate-0"
-            }`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707"
-            />
-          </svg>
-
-          {/* Ícone da Lua */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={`absolute w-6 h-6 transition-all duration-300 text-white ${
-              isDarkMode ? "opacity-100 rotate-0" : "opacity-0 -rotate-90"
-            }`}
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-            />
-          </svg>
+        <div
+          className={`absolute top-1/2 transform -translate-y-1/2 w-8 flex justify-center items-center transition-all duration-300 ${
+            isDarkMode ? "left-10" : "left-1"
+          }`}
+        >
+          {isDarkMode ? (
+            <FaMoon className="text-white text-lg" />
+          ) : (
+            <FaSun className="text-yellow-500 text-lg" />
+          )}
         </div>
       </button>
     </div>
